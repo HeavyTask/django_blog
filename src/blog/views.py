a9b1b2ea0b994776ff93db13779d9136ec8007cf
unknown <expelmahmud@gmail.com>
2020-04-15 13:23:55 +0600

    Blog list View & Detailview Added
    Blog List View & Detailview added using generic class based views

---
+++ src/blog/views.py
@@ -0,0 +1,12 @@
+from django.views.generic import ListView, DetailView
+
+from .models import Entry
+
+
+class HomeView(ListView):
+    template_name = "index.html"
+    queryset = Entry.objects.order_by('-created_at')
+
+
+class EntryDetailView(DetailView):
+    model = Entry
